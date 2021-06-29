3cb20814e09700cade4bbbf3ebd3ba61f630b8f9
Junting Wu <563403122@qq.com>
2021-06-29 17:20:14 +0800

    Add files via upload

--- Data/process_data.py
+++ Data/process_data.py
@@ -1,16 +1,40 @@
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(pat = ';', expand = True)
+    row = categories.iloc[[1]]
+    category_colnames = [category_name.split('-')[0] for category_name in row.values[0]]
+    categories.columns = category_colnames
+    
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(np.int)
+     
+    df = df.drop('categories', axis =1)
+    df = pd.concat([df, categories],axis =1)
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+ database_filename)
+    table_name = database_filename.replace(".db","")
+    print(table_name)
+    df.to_sql(table_name, engine, index = False, if_exists='replace')
+    
+    
+    
 
 
 def main():
